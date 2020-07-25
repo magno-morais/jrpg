d92f39f5927439a9093654ca6c55d46e9ca9339d
magno-morais <carlosviniciusjose@hotmail.com>
2020-07-25 02:07:05 -0300

    made basic character mov and minimun map design

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosix;
+    public Vector2 minPosix;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void LateUpdate(){
+        if(transform.position != target.position){
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosix.x, maxPosix.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosix.y, maxPosix.y);
+
+            transform.position = Vector3.Lerp(
+                transform.position,
+                targetPosition,
+                smoothing);
+        }
+    }
+}
